f782338ed9afdc2f38f7da70c3af573d44c6882a
R-Walmsley <rosemary.walmsley@gtc.ox.ac.uk>
2021-11-24 11:47:07 +0000

    tidy tutorials

--- R/utils.R
+++ R/utils.R
@@ -22,7 +22,7 @@ plot_var_and_quintile <- function(data, exposure, outcome) {
     ylab = outcome,
     xlim = c(0, quantile(exp_var, c(0.99), na.rm = TRUE))
   )
-  
+
   boxplot(
     out_var ~ exp_var_quintile,
     main = paste(exposure, '-quintile', sep = ""),
@@ -44,44 +44,43 @@ plot_average_day <-
            exposure_prefix,
            exposure_suffix,
            y_label = exposure_prefix) {
-    for (name in c("hr_PAcols",
-                   "mean_PAcols",
-                   "se_PAcols",
-                   "low_PAcols",
-                   "high_PAcols")) {
+    for (name in c("hr_pa_cols",
+                   "mean_pa_cols",
+                   "se_pa_cols",
+                   "low_pa_cols",
+                   "high_pa_cols")) {
       assign(name, rep(NA, times = 24))
     }
     hrs <- rep(NA, times = 24)
     for (hr in 0:23) {
       # Admin
       hrs[hr + 1] <- hr
-      hr_PACols[hr + 1] <-
-        paste0(exposurePrefix, hr, exposureSuffix)
-      data_col_full <- data[, hrPACols[hr + 1]]
+      hr_pa_cols[hr + 1] <-
+        paste0(exposure_prefix, hr, exposure_suffix)
+      data_col_full <- data[, hr_pa_cols[hr + 1]]
       data_col <- data_col_full[!is.na(data_col_full)]
       n <- length(data_col)
-      
+
       # Means and SEs
-      mean_PACols[hr + 1] <- mean(data_col)
-      se_PACols[hr + 1] <- sqrt(var(data_col))/sqrt(n)
-      low_PACols <- mean_PACols[hr + 1] - 1.96 * se_PACols[hr + 1]
-      high_PACols <- mean_PACols[hr + 1] + 1.96 * se_PACols[hr + 1]
-    
+      mean_pa_cols[hr + 1] <- mean(data_col)
+      se_pa_cols[hr + 1] <- sqrt(var(data_col))/sqrt(n)
+      low_pa_cols[hr + 1] <- mean_pa_cols[hr + 1] - 1.96 * se_pa_cols[hr + 1]
+      high_pa_cols[hr + 1] <- mean_pa_cols[hr + 1] + 1.96 * se_pa_cols[hr + 1]
+
       }
-    
+
     # Draw plot
-    plot_data <- data.frame(cbind(hrs, mean_PACols, low_PACols, high_PACols))
+    plot_data <- data.frame(cbind(hrs, mean_pa_cols, low_pa_cols, high_pa_cols))
     plot <-
       ggplot2::ggplot(data = plot_data,
-                      aes(x = hrs, y = mean_PACols)) +
+                      aes(x = hrs, y = mean_pa_cols)) +
+      geom_ribbon(aes(x = hrs, ymin = low_pa_cols,
+                      ymax = high_pa_cols), fill = "grey") +
       geom_line() +
-      geom_ribbon(aes(x = hrs, ymin = low_PACols,
-                      ymax = high_PACols), colour = "grey") +
-      
       labs(title = "Time-of-day behaviour profile",
-           y = yAxisLabel,
+           y = y_label,
            x = "Hour of Day")
-    
+
     return(plot)
-    
+
   }
